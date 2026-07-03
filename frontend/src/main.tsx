33f73af9289d523a15bcb7e8f114aced9ec3cad1
poti <poti@kaoz.org>
2026-07-03 07:19:16 +0200

    fix: wrong port

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,7 +11,7 @@ import { configureOidc } from "@admin/providers/authProvider.ts";
 import * as Sentry from "@sentry/react";
 
 const log = createLogger("Bootstrapper");
-const API_HOST = import.meta.env.VITE_API_HOST ?? "http://localhost:310";
+const API_HOST = import.meta.env.VITE_API_HOST ?? "http://localhost:3100";
 
 export async function bootstrapApp() {
   const rootElement = document.getElementById("root");
